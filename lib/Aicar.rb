f3c90fb44863a636c0fee9a886f2929a67347867
carper33 <carla.lahoz.perez@gmail.com>
2014-11-05 13:01:13 +0000

    sets attributes

--- lib/Aicar.rb
+++ lib/Aicar.rb
@@ -7,7 +7,39 @@ module Aicar
 
 		end
 		def content
-			IO.read(@file)
+			@content = IO.read(@file)
+		end
+		def dialogs
+			content.split("\r\n\r\n").map do |dialog|
+				lines = dialog.split("\r\n")
+				id = lines.delete_at(0)
+				time =  lines.delete_at(0)
+				text = lines.join("\r\n")
+				start_time, end_time = time.split(" --> ")
+				Dialog.new(start_time,end_time,text,id)
+			end
+
+		end
+	end
+	class Dialog
+		def initialize(start_time,end_time,text,id)
+			@start_time = start_time
+			@end_time = end_time
+			@text = text
+			@id = id
+
+		end
+		def start_time
+			@start_time
+		end
+		def end_time
+			@end_time
+		end	
+		def id
+			@id
+		end	
+		def text
+			@text
 		end
 	end
 end
--- spec/Aicar_spec.rb
+++ spec/Aicar_spec.rb
@@ -5,6 +5,23 @@ module Aicar
 		it "leer subtitulos" do
 			subtitle = Subtitle.new("/vagrant/ruby/Oriol/TDD/Aicar/spec/The_Walking.srt")
 			expect(subtitle.content).to include("Little girl.")
+		end
+		it "does something" do
+			subtitle = Subtitle.new("/vagrant/ruby/Oriol/TDD/Aicar/spec/The_Walking.srt")
+			expect(subtitle.dialogs.length).to eq(463)
+		end
+		it "sets the dialogs attributes" do
+			subtitle = Subtitle.new("/vagrant/ruby/Oriol/TDD/Aicar/spec/The_Walking.srt")
+			dialog= subtitle.dialogs.first
+			expect(dialog.start_time).to eq("00:02:26,371")
+			expect(dialog.end_time).to eq("00:02:36,570")
+			expect(dialog.text).to eq("Sync by YYeTs.net\r\nwww.addic7ed.com")
+			expect(dialog.id).to eq("1")
+		end	
+	end
+	describe Dialog do
+		it "Process Dialogs" do			
+		
 		end	
 	end
 end	
